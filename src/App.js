61f80e50c80a91115280f1165528a303426e8058
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-09-18 16:07:29 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import HomeSlider from './components/HomeSlider'
 
 const App = () => {
   return (
-    <div>
-      hy
+    <div className='App'>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-md-12 px-8'>
+                    <Header/>
+                    <HomeSlider/>
+                </div>
+            </div>
+        </div>
+      <Header/>
     </div>
   )
 }
